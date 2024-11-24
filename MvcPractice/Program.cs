6fa7f82af19e56f3ece39e97e917582421df2e5c
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-11-24 13:53:50 +0300

    .

--- MvcPractice/Program.cs
+++ MvcPractice/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// MVC servislerini ekliyoruz
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// wwwroot klasöründeki statik dosyalarý kullanabilmek için gerekli middleware
+app.UseStaticFiles();
 
+// Routing yapýlandýrmasý
+app.UseRouting();
+
+// Varsayýlan route ayarlarý
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=CustomerOrders}/{action=Index}/{id?}");
+
+// Uygulamayý çalýþtýr
 app.Run();
